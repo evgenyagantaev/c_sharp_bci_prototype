88fdf9c41716945c15fc4775dd5fa136b01b7bc3
evgenyagantaev <agantaev@mail.ru>
2023-06-22 23:23:06 +0300

    todo - write characteristic access;

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using System.Threading;
 using System.ComponentModel.Design;
 using Windows.Storage.Streams;
+using System.Collections.Generic;
 
 namespace c_sharp_bci_prototype
 {
@@ -17,7 +18,69 @@ namespace c_sharp_bci_prototype
     public static class Program
     {
         // Devices found by watcher
-        private readonly static Hashtable s_foundDevices = new Hashtable();
+        //private readonly static Hashtable s_foundDevices = new Hashtable();
+
+        static List <BluetoothLEDevice> foundDevices = new List<BluetoothLEDevice>();
+
+        //public static void Main(string[] args) 
+        //{
+        //    _ = async_operation_test();
+
+        //    int i = 0;
+        //    while (true) 
+        //    {
+        //        Console.WriteLine("Main " + i.ToString());
+        //        Thread.Sleep(1000); // Wait for 1 second
+        //        i++;
+        //    }
+        //}
+
+        //public static async Task async_operation_test() // асинхронный метод
+        //{
+        //    BeforeCall();
+        //    Task task = OperationAsync(); //асинхронная операция
+        //    //Task task = Task.Delay(7000); //асинхронная операция
+        //    AfterCall();
+        //    await task;
+        //    AfterAwait();
+        //}
+
+        //private static void BeforeCall()
+        //{
+        //    for(int i=0; i<3; i++)
+        //    {
+        //        Console.WriteLine("BeforeCall " + i.ToString());
+        //        Thread.Sleep(1000); // Wait for 1 second
+        //    }
+        //}
+
+        //private static void AfterCall()
+        //{
+        //    for (int i = 0; i < 3; i++)
+        //    {
+        //        Console.WriteLine("AfterCall " + i.ToString());
+        //        Thread.Sleep(1000); // Wait for 1 second
+        //    }
+        //}
+
+        //private static void AfterAwait()
+        //{
+        //    for (int i = 0; i < 3; i++)
+        //    {
+        //        Console.WriteLine("AfterAwait " + i.ToString());
+        //        Thread.Sleep(1000); // Wait for 1 second
+        //    }
+        //}
+
+        //public static async Task OperationAsync()
+        //{
+        //    for (int i = 0; i < 7; i++)
+        //    {
+        //        Console.WriteLine("OperationAsync " + i.ToString());
+        //        await Task.Delay(1000);
+        //    }
+        //}
+
         public static void Main()
         {
             Console.WriteLine($"BCI C# NB2 prototype.");
@@ -26,40 +89,31 @@ namespace c_sharp_bci_prototype
             BluetoothLEAdvertisementWatcher watcher = new BluetoothLEAdvertisementWatcher();
             watcher.ScanningMode = BluetoothLEScanningMode.Active;
             watcher.Received += Watcher_ReceivedAsync;
+            Console.WriteLine("Starting BluetoothLEAdvertisementWatcher");
+            Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+            watcher.Start();
+
+            while (foundDevices.Count == 0)
+            {
+                Thread.Sleep( 100 );   
+            }
+
+            Console.WriteLine("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+            Console.WriteLine("Stopping BluetoothLEAdvertisementWatcher");
+            // We can't connect if watch running so stop it.
+            watcher.Stop();
+
+            Console.WriteLine();
+            Console.WriteLine($"Devices found = {foundDevices.Count}");
+            Console.WriteLine();
+            Console.WriteLine($"---------------------------------------");
+
+            _ = ConnectAndReceiveSomeAsync(foundDevices[0]);
+            Console.WriteLine($"---------------------------------------");
 
             while (true)
             {
-                Console.WriteLine("Starting BluetoothLEAdvertisementWatcher");
-                Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-                watcher.Start();
 
-                // Run until we have found some devices to connect to
-                while (s_foundDevices.Count == 0)
-                {
-                    Thread.Sleep(5000);
-                }
-
-                Console.WriteLine("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
-                Console.WriteLine("Stopping BluetoothLEAdvertisementWatcher");
-
-                // We can't connect if watch running so stop it.
-                watcher.Stop();
-
-                Console.WriteLine();
-                Console.WriteLine($"Devices found = {s_foundDevices.Count}");
-                Console.WriteLine();
-                Console.WriteLine($"---------------------------------------");
-                //Console.WriteLine("Connecting and Reading data");
-
-                foreach (DictionaryEntry entry in s_foundDevices)
-                {
-                    BluetoothLEDevice device = entry.Value as BluetoothLEDevice;
-
-                    _ = ConnectAndReceiveSomeAsync(device);
-                }
-                Console.WriteLine($"---------------------------------------");
-
-                s_foundDevices.Clear();
             }
 
         }
@@ -71,14 +125,15 @@ namespace c_sharp_bci_prototype
 
         private static async Task ProcessWatcherReceivedAsync(BluetoothLEAdvertisementWatcher sender, BluetoothLEAdvertisementReceivedEventArgs args)
         {
+            Console.WriteLine("Found " + args.Advertisement.LocalName + " " + $"address :{args.BluetoothAddress:X}");
+
             if (IsValidDevice(args))
             {
-                //Console.WriteLine($"Found an NB2 eeg amplifyer :{args.BluetoothAddress:X}");
+                Console.WriteLine($"Found an NB2 eeg amplifyer :{args.BluetoothAddress:X}");
 
                 // Add it to list as a BluetoothLEDevice
-                //BluetoothLEDevice dev = BluetoothLEDevice.FromBluetoothAddress(args.BluetoothAddress, args.BluetoothAddressType);
                 BluetoothLEDevice dev = await BluetoothLEDevice.FromBluetoothAddressAsync(args.BluetoothAddress);
-                s_foundDevices.Add(args.BluetoothAddress, dev);
+                foundDevices.Add(dev);
             }
         }
 
@@ -103,7 +158,7 @@ namespace c_sharp_bci_prototype
                 {
                     Console.WriteLine($"{service.Uuid}");
                     Console.WriteLine($"===========================");
-                    
+
                     var characteristics_result = await service.GetCharacteristicsAsync();
                     if (characteristics_result.Status == GattCommunicationStatus.Success)
                     {
@@ -115,10 +170,105 @@ namespace c_sharp_bci_prototype
                     }
                     Console.WriteLine($"===========================");
                 }
+
+                Console.WriteLine($"===========================");
+                Console.WriteLine($"^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+
+                var sr = await device.GetGattServicesForUuidAsync(GenericAccess);
+                var cr = await sr.Services[0].GetCharacteristicsForUuidAsync(DeviceNameString);
+                var gc = cr.Characteristics[0];
+                var rr = await gc.ReadValueAsync();
+                var rdr = DataReader.FromBuffer(rr.Value);
+                Console.WriteLine($"Name: {rdr.ReadString(14)}");
+
+                sr = await device.GetGattServicesForUuidAsync(DeviceInformation);
+                cr = await sr.Services[0].GetCharacteristicsForUuidAsync(SerialNumberString);
+                gc = cr.Characteristics[0];
+                rr = await gc.ReadValueAsync();
+                rdr = DataReader.FromBuffer(rr.Value);
+                Console.WriteLine($"Serial: {rdr.ReadString(4)}");
+
+                sr = await device.GetGattServicesForUuidAsync(BatteryService);
+                cr = await sr.Services[0].GetCharacteristicsForUuidAsync(BatteryPropertiesUuid);
+                gc = cr.Characteristics[0];
+                rr = await gc.ReadValueAsync();
+                rdr = DataReader.FromBuffer(rr.Value);
+
+                rdr.ReadBytes(value: PropertyBytes);
+                BatteryProperties batteryProperties;
+                batteryProperties.Capacity = PropertyBytes[1];
+                batteryProperties.Capacity = (UInt16)(batteryProperties.Capacity << 8);
+                batteryProperties.Capacity += PropertyBytes[0];
+                batteryProperties.Level = PropertyBytes[3];
+                batteryProperties.Level = (UInt16)(batteryProperties.Level << 8);
+                batteryProperties.Level += PropertyBytes[2];
+                batteryProperties.Voltage = PropertyBytes[5];
+                batteryProperties.Voltage = (UInt16)(batteryProperties.Voltage << 8);
+                batteryProperties.Voltage += PropertyBytes[4];
+                batteryProperties.Current = PropertyBytes[7];
+                batteryProperties.Current = (Int16)(batteryProperties.Current << 8);
+                batteryProperties.Current += PropertyBytes[6];
+                batteryProperties.Temperature = PropertyBytes[9];
+                batteryProperties.Temperature = (Int16)(batteryProperties.Temperature << 8);
+                batteryProperties.Temperature += PropertyBytes[8];
+                Console.WriteLine($"capacity: {batteryProperties.Capacity}");
+                Console.WriteLine($"level: {batteryProperties.Level}");
+                Console.WriteLine($"voltage: {batteryProperties.Voltage}");
+                Console.WriteLine($"current: {batteryProperties.Current}");
+                Console.WriteLine($"temperature: {batteryProperties.Temperature}");
+
+                sr = await device.GetGattServicesForUuidAsync(ControlServiceUuid);
+                Console.WriteLine($"Control service result status: {sr.Status}");
+                GattDeviceService control_service = sr.Services[0];
+                Console.WriteLine($"Control service uuid: {control_service.Uuid}");
+                Console.WriteLine($"Try to get access to command uuid: {CommandUuid}");
+                cr = await control_service.GetCharacteristicsForUuidAsync(CommandUuid);
+                Console.WriteLine($"Command characteristic result status: {cr.Status}");
+                gc = cr.Characteristics[0];
+                Console.WriteLine($"command characteristic uuid: {gc.Uuid}");
+
+                string[] hexValues;
+                DataWriter dw;
+                byte[] byteArray;
+                GattWriteResult wr;
+
             }
 
         }
 
+        public struct BatteryProperties
+        {
+            public UInt16 Capacity;     // mAh
+            public UInt16 Level;    // % * 10
+            public UInt16 Voltage;  // mV
+            public Int16 Current;   // mA
+            public Int16 Temperature; // °C * 10
+        }
+
+        static Guid GenericAccess = new Guid("00001800-0000-1000-8000-00805f9b34fb");
+        static Guid DeviceNameString = new Guid("00002a00-0000-1000-8000-00805f9b34fb");
+
+        static Guid DeviceInformation = new Guid("0000180a-0000-1000-8000-00805f9b34fb");
+        static Guid SerialNumberString = new Guid("00002a25-0000-1000-8000-00805f9b34fb");
+
+        static Guid BatteryService = new Guid("0000180f-0000-1000-8000-00805f9b34fb");
+        static Guid BatteryPropertiesUuid = new Guid("5c979c9f-a1ac-5715-9a1b-f81a581179d9");
+        const int BatteryPropertiesSize = 10;
+        static byte[] PropertyBytes = new byte[BatteryPropertiesSize];
+
+        static Guid CurrentDateServUuid = new Guid("00001805-0000-1000-8000-00805f9b34fb");
+        static Guid DateUuid = new Guid("a9b157a1-5827-5553-9ba5-5f5ff8f8e173");
+        const int DateSize = 4;
+        static byte[] DateBytes = new byte[DateSize];
+
+        static Guid ControlServiceUuid = new Guid("a183c5a7-1e93-8deb-a113-e8d5bb5581db");
+        static Guid CommandUuid = new Guid("7395ca15-5997-5a1b-a138-75a7a573b8e5");
+
+        static readonly string start_acquisition_data_command = "01 00 00 01 00 FF FF 00 02 00 00 00 00 00 00 00 02 00 59 6D 3D B3 DC 5C 63 B8 1B 91 D2 76 2D E7 C5 28 DA A8 95 57 18 53 32 55 8B 7E 97 3C A6 A6 0D 99";
+        static readonly string start_acquisition_impedance_command = "01 01 00 01 00 FF FF 00 02 00 00 00 00 00 00 00 02 00 3E 73 58 12 CE 8C 90 B1 B0 AB 03 20 35 C2 06 5A 0F C3 AE 3F BB C8 43 E6 24 FC 0F 9C 23 5A 49 70";
+        static readonly string stop_acquisition_data_command = "02 8A DC 88 98 EC A3 3B 08 CB BD 40 12 50 FC 6C EA 4E FF 7D 01 C7 87 DC 69 9A 76 52 18 7F FF D5 21";
+        static readonly string connection_close_command = "03 67 ED AD EE C5 5D AF 5D A2 FB DC C5 8C 49 62 22 4E 63 64 ED A9 50 AC 9E 58 DF 70 77 CC 08 E2 BC";
+        static readonly string turn_off_command = "05 0D DE C2 81 BC 8B 45 00 68 68 47 03 C7 6C B7 DA C0 49 C8 C1 C0 40 82 60 D7 D7 5B EE D9 4B A8 F8 0E";
 
 
     }
