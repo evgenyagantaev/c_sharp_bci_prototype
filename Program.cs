8a28a74fcce75494461ea3ffe6e5f66d1766330d
evgenyagantaev <agantaev@mail.ru>
2023-06-23 14:18:40 +0300

    read samples on notification does not work;

--- Program.cs
+++ Program.cs
@@ -217,6 +217,13 @@ namespace c_sharp_bci_prototype
                 Console.WriteLine($"current: {batteryProperties.Current}");
                 Console.WriteLine($"temperature: {batteryProperties.Temperature}");
 
+                sr = await device.GetGattServicesForUuidAsync(AcquisitionServiceUuid);
+                cr = await sr.Services[0].GetCharacteristicsForUuidAsync(DataUuid);
+                Console.WriteLine($"get data characteristic result status: {cr.Status}");
+                var data_characteristic = cr.Characteristics[0];
+                // subscribe on notification
+                data_characteristic.ValueChanged += ReceiveData;
+
                 sr = await device.GetGattServicesForUuidAsync(ControlServiceUuid);
                 Console.WriteLine($"Control service result status: {sr.Status}");
                 GattDeviceService control_service = sr.Services[0];
@@ -227,61 +234,49 @@ namespace c_sharp_bci_prototype
                 gc = cr.Characteristics[0];
                 Console.WriteLine($"command characteristic uuid: {gc.Uuid}");
 
-                string[] hexValues;
+                
+
                 DataWriter dw;
-                byte[] byteArray;
                 GattWriteResult wr;
 
                 // start data acquisition
-                hexValues = start_acquisition_data_command.Split(' ');
-                byteArray = new byte[hexValues.Length];
-                for (int i = 0; i < hexValues.Length; i++)
-                {
-                    byteArray[i] = Convert.ToByte(hexValues[i], 16);
-                }
                 dw = new DataWriter();
-                dw.WriteBytes(byteArray);
+                dw.WriteBytes(hexvals_to_bytes(start_acquisition_data_command));
                 wr = await gc.WriteValueWithResultAsync(dw.DetachBuffer(), GattWriteOption.WriteWithResponse);
                 Console.WriteLine($"Write result status: {wr.Status}");
 
-                // stop data acquisition
-                hexValues = stop_acquisition_data_command.Split(' ');
-                byteArray = new byte[hexValues.Length];
-                for (int i = 0; i < hexValues.Length; i++)
-                {
-                    byteArray[i] = Convert.ToByte(hexValues[i], 16);
-                }
-                dw = new DataWriter();
-                dw.WriteBytes(byteArray);
-                wr = await gc.WriteValueWithResultAsync(dw.DetachBuffer(), GattWriteOption.WriteWithResponse);
-                Console.WriteLine($"Write result status: {wr.Status}");
+                //// stop data acquisition
+                //dw = new DataWriter();
+                //dw.WriteBytes(hexvals_to_bytes(stop_acquisition_data_command));
+                //wr = await gc.WriteValueWithResultAsync(dw.DetachBuffer(), GattWriteOption.WriteWithResponse);
+                //Console.WriteLine($"Write result status: {wr.Status}");
 
-                // turn off command
-                hexValues = turn_off_command.Split(' ');
-                byteArray = new byte[hexValues.Length];
-                for (int i = 0; i < hexValues.Length; i++)
-                {
-                    byteArray[i] = Convert.ToByte(hexValues[i], 16);
-                }
-                dw = new DataWriter();
-                dw.WriteBytes(byteArray);
-                wr = await gc.WriteValueWithResultAsync(dw.DetachBuffer(), GattWriteOption.WriteWithResponse);
-                Console.WriteLine($"Write result status: {wr.Status}");
+                //// turn off command
+                //dw = new DataWriter();
+                //dw.WriteBytes(hexvals_to_bytes(turn_off_command));
+                //wr = await gc.WriteValueWithResultAsync(dw.DetachBuffer(), GattWriteOption.WriteWithResponse);
+                //Console.WriteLine($"Write result status: {wr.Status}");
 
-                // connection close command
-                hexValues = connection_close_command.Split(' ');
-                byteArray = new byte[hexValues.Length];
-                for (int i = 0; i < hexValues.Length; i++)
-                {
-                    byteArray[i] = Convert.ToByte(hexValues[i], 16);
-                }
-                dw = new DataWriter();
-                dw.WriteBytes(byteArray);
-                wr = await gc.WriteValueWithResultAsync(dw.DetachBuffer(), GattWriteOption.WriteWithResponse);
-                Console.WriteLine($"Write result status: {wr.Status}");
+                //// connection close command
+                //dw = new DataWriter();
+                //dw.WriteBytes(hexvals_to_bytes(connection_close_command));
+                //wr = await gc.WriteValueWithResultAsync(dw.DetachBuffer(), GattWriteOption.WriteWithResponse);
+                //Console.WriteLine($"Write result status: {wr.Status}");
 
             }
 
+        }
+
+        private static byte[] hexvals_to_bytes(string hex_values)
+        {
+            var hexValues = hex_values.Split(' ');
+            var byteArray = new byte[hexValues.Length];
+            for (int i = 0; i < hexValues.Length; i++)
+            {
+                byteArray[i] = Convert.ToByte(hexValues[i], 16);
+            }
+
+            return byteArray;
         }
 
 
@@ -294,101 +289,110 @@ namespace c_sharp_bci_prototype
 
         // Array of previous values
         static Int32[] prev = new Int32[Pkt.ChannelsCount];
+
+        static int counter = 0;
         static private void ReceiveData(GattCharacteristic sender, GattValueChangedEventArgs args)
         {
-            int val;
-            int offset = 0;
-            UInt32 code = 0;
-
-            // Array of decoded values
-            int[] array = new int[Pkt.ChannelsCount * Pkt.SamplesCount];
-            int pos = 0;
-
-            // Flag indicates that the channel is disabled
-            bool skip;
-
-            // An Indicate or Notify reported that the value has changed
-            var reader = DataReader.FromBuffer(args.CharacteristicValue);
-
-            // Parse the data however required
-            byte[] input = new byte[reader.UnconsumedBufferLength];
-
-            // Read input
-            reader.ReadBytes(input);
-
-            UInt16 counter = BitConverter.ToUInt16(input, input.Length - 2);
-
-            pktCounter++;
-
-            if (pktCounter != counter)
-            {
-                errorsCount++;
-            }
-
-            for (int i = 0; i < Pkt.SamplesCount; i++)
-            {
-                if (i != 0)
-                {
-                    code = BitConverter.ToUInt32(input, offset);
-                    offset += 4;
-                }
-
-                for (int ch = 0; ch < Pkt.ChannelsCount; ch++)
-                {
-                    skip = ((EnabledChannels >> ch) & 1) == 0;
-
-                    if (i == 0)
-                    {
-                        if (!skip)
-                        {
-                            val = (input[offset + 2] << 8) | (input[offset + 1] << 16) | (input[offset] << 24);
-                            val /= 256;
-                            offset += 3;
-                        }
-                        else
-                        {
-                            val = Int32.MaxValue;
-                        }
-                    }
-                    else
-                    {
-                        if (!skip)
-                        {
-                            switch ((code >> ch * 2) & 3)
-                            {
-                                case 0:
-                                    val = prev[ch] + (sbyte)input[offset];
-                                    offset += 1;
-                                    break;
-
-                                case 1:
-                                    val = prev[ch] + BitConverter.ToInt16(input, offset);
-                                    offset += 2;
-                                    break;
-
-                                case 2:
-                                    val = (input[offset + 2] << 8) | (input[offset + 1] << 16) | (input[offset] << 24);
-                                    val /= 256;
-                                    offset += 3;
-                                    break;
-
-                                default:
-                                    val = Int32.MaxValue;
-                                    break;
-                            }
-                        }
-                        else
-                        {
-                            val = Int32.MaxValue;
-                        }
-                    }
-
-                    array[pos] = val;
-                    prev[ch] = val;
-                    pos++;
-                }
-            }
+            Console.WriteLine($"value changed: {counter}");
+            counter++;
         }
+
+
+        //static private void ReceiveData(GattCharacteristic sender, GattValueChangedEventArgs args)
+        //{
+        //    int val;
+        //    int offset = 0;
+        //    UInt32 code = 0;
+
+        //    // Array of decoded values
+        //    int[] array = new int[Pkt.ChannelsCount * Pkt.SamplesCount];
+        //    int pos = 0;
+
+        //    // Flag indicates that the channel is disabled
+        //    bool skip;
+
+        //    // An Indicate or Notify reported that the value has changed
+        //    var reader = DataReader.FromBuffer(args.CharacteristicValue);
+
+        //    // Parse the data however required
+        //    byte[] input = new byte[reader.UnconsumedBufferLength];
+
+        //    // Read input
+        //    reader.ReadBytes(input);
+
+        //    UInt16 counter = BitConverter.ToUInt16(input, input.Length - 2);
+
+        //    pktCounter++;
+
+        //    if (pktCounter != counter)
+        //    {
+        //        errorsCount++;
+        //    }
+
+        //    for (int i = 0; i < Pkt.SamplesCount; i++)
+        //    {
+        //        if (i != 0)
+        //        {
+        //            code = BitConverter.ToUInt32(input, offset);
+        //            offset += 4;
+        //        }
+
+        //        for (int ch = 0; ch < Pkt.ChannelsCount; ch++)
+        //        {
+        //            skip = ((EnabledChannels >> ch) & 1) == 0;
+
+        //            if (i == 0)
+        //            {
+        //                if (!skip)
+        //                {
+        //                    val = (input[offset + 2] << 8) | (input[offset + 1] << 16) | (input[offset] << 24);
+        //                    val /= 256;
+        //                    offset += 3;
+        //                }
+        //                else
+        //                {
+        //                    val = Int32.MaxValue;
+        //                }
+        //            }
+        //            else
+        //            {
+        //                if (!skip)
+        //                {
+        //                    switch ((code >> ch * 2) & 3)
+        //                    {
+        //                        case 0:
+        //                            val = prev[ch] + (sbyte)input[offset];
+        //                            offset += 1;
+        //                            break;
+
+        //                        case 1:
+        //                            val = prev[ch] + BitConverter.ToInt16(input, offset);
+        //                            offset += 2;
+        //                            break;
+
+        //                        case 2:
+        //                            val = (input[offset + 2] << 8) | (input[offset + 1] << 16) | (input[offset] << 24);
+        //                            val /= 256;
+        //                            offset += 3;
+        //                            break;
+
+        //                        default:
+        //                            val = Int32.MaxValue;
+        //                            break;
+        //                    }
+        //                }
+        //                else
+        //                {
+        //                    val = Int32.MaxValue;
+        //                }
+        //            }
+
+        //            array[pos] = val;
+        //            prev[ch] = val;
+        //            pos++;
+        //        }
+        //    }
+        //}
 
         public struct BatteryProperties
         {
@@ -423,6 +427,9 @@ namespace c_sharp_bci_prototype
 
         static Guid ControlServiceUuid = new Guid("a183c5a7-1e93-8deb-a113-e8d5bb5581db");
         static Guid CommandUuid = new Guid("7395ca15-5997-5a1b-a138-75a7a573b8e5");
+
+        static Guid AcquisitionServiceUuid = new Guid("5775ab91-ee53-57e1-a77b-1c87183bd78c");
+        static Guid DataUuid = new Guid("75851135-953a-7739-c781-5a935531397a");
 
         static readonly string start_acquisition_data_command = "01 00 00 01 00 FF FF 00 02 00 00 00 00 00 00 00 02 00 59 6D 3D B3 DC 5C 63 B8 1B 91 D2 76 2D E7 C5 28 DA A8 95 57 18 53 32 55 8B 7E 97 3C A6 A6 0D 99";
         static readonly string start_acquisition_impedance_command = "01 01 00 01 00 FF FF 00 02 00 00 00 00 00 00 00 02 00 3E 73 58 12 CE 8C 90 B1 B0 AB 03 20 35 C2 06 5A 0F C3 AE 3F BB C8 43 E6 24 FC 0F 9C 23 5A 49 70";
